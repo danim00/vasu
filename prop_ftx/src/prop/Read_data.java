78050c79989c359bb4b96e35c3dd1acf07d6ed85
Vasu Bhatnagar <Vasu Bhatnagar@192.168.3.30>
2023-02-02 17:31:07 +0530

    second commit

--- prop_ftx/src/prop/Read_data.java
+++ prop_ftx/src/prop/Read_data.java
@@ -49,24 +49,49 @@ public class Read_data {
 		  
 		  //Step1 - To switch on the web table of module
 		  WebElement frame1 = driver.findElement(By.xpath("//*[@id='dashboard-analytics']/div/div/div/div[3]"));
-		  driver.switchTo().frame(frame1);
+		//driver.switchTo().frame(frame1);
 		  System.out.println("switch successful :" +"frame1");
 		  
 		  
 		  //Step2 - To get the number of rows
-		List<WebElement> rowelements = driver.findElements(By.xpath("//*[@id=\"dashboard-analytics\"]/div/div/div/div[3]/div[1]/div/div/div[2]"));
+		List<WebElement> rowelements = driver.findElements(By.xpath("//*[@id='dashboard-analytics']/div/div/div/div[3]/div[1]/div/div/div[2]"));
 		System.out.println(rowelements.size());
 		int rowsize = rowelements.size();
 		
 		 //Step2 - To get the number of column
-		List<WebElement> columnelements = driver.findElements(By.xpath("//*[@id=\"dashboard-analytics\"]/div/div/div/div[3]/div[1]/div/div/div[1]/div"));
+		List<WebElement> columnelements = driver.findElements(By.xpath("//*[@id='dashboard-analytics']/div/div/div/div[3]"));
 		System.out.println(columnelements.size());
 		int columnsize = columnelements.size();
 		  
 		 // Step4- To Get data from the row
+		WebElement row6 = driver.findElement(By.xpath("//*[@id=\"row-6\"]"));
 		
+		// step5 - Name
+		WebElement name = driver.findElement(By.xpath("//span[normalize-space()='Bhumica']"));
+		System.out.println(name.getText());
+		String username = name.getText();
 		
-		//driver.close();
+		// step6 - Email
+		WebElement email = driver.findElement(By.xpath("//div[contains(text(),'Bhumica@yopmail.com')]"));
+		System.out.println(email.getText());
+		String email1 = email.getText();
+				
+		// step7 - number
+		WebElement phnumber = driver.findElement(By.xpath("//div[contains(text(),'1234567892')]"));
+		System.out.println(phnumber.getText());
+		String phn = phnumber.getText();
+		
+		// step7 - city
+		WebElement city = driver.findElement(By.xpath("//div[contains(text(),'Along')]"));
+		System.out.println(city.getText());
+		String citydetail = city.getText();
+		
+		// step7 - lastlogin
+		WebElement lastlogin = driver.findElement(By.xpath("//div[@id='row-6']//div[@id='cell-6-undefined']//div[@data-tag='allowRowEvents']"));
+		System.out.println(lastlogin.getText());
+		String login = lastlogin.getText();
+			
+		driver.close();
 		
 		
 
